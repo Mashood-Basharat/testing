1a434263f1c1381c01df3e826293bb4071a023bb
Mashood-Basharat <mashoodbasharat7890@gmail.com>
2024-11-13 14:12:00 +0500

    commit

--- app.py
+++ app.py
@@ -27,17 +27,5 @@ def setting():
 
 
 
-
-
-
-# Logout Route
-@app.route('/logout')
-def logout():
-    session.clear()
-    flash("You have been logged out.", "info")
-    return redirect(url_for('login'))
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
